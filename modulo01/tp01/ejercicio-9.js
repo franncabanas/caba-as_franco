69609a6561c110d4cf9c6a32e24842874a060f16
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:16:55 -0300

    Create ejercicio-9.js

---
+++ modulo01/tp01/ejercicio-9.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> ejercicio 9
+        </title>
+    </head>
+    <body>
+        <script>
+            var vocales=["A","E", "I","O", "U"];
+            
+            document.write(vocales [0]);
+            document.write("</br>")
+            document.write(vocales[1] );
+            document.write("</br>")
+            document.write(vocales[2] );
+            document.write("</br>")
+            document.write(vocales[3] );
+            document.write("</br>")
+            document.write(vocales[4] );
+    
+        </script>
+    </body>
+</html>
