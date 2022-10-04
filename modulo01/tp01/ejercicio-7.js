8b96044720c879daff811b314acd4b968dc9883f
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:16:22 -0300

    Create ejercicio-7.js

---
+++ modulo01/tp01/ejercicio-7.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+let dato, resultado;
+<!--se decaran las variables-->
+val1 = window.prompt("Introduce tu nombre", "...");
+<!--solicita que se ingrese por teclado el nombre-->
+val2 = window.prompt("Introduce tu apellido", "...");
+<!--solicita que se ingrese por teclado el apellido-->
+resultado = `Concatenado tu nombre y apellido es: ${val1} ${val2} ` ;
+<!-- concatenas el nombre y el apellido para mostrarlo en un mensaje-->
+document.write(resultado);
+<!--escribe la cadena de texto -->
+</script>
+</body>
+</html
