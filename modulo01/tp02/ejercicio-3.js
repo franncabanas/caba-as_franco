ceb81133d5764fbe62caea94519c26f7b63084af
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:22:00 -0300

    Create ejercicio-3.js

---
+++ modulo01/tp02/ejercicio-3.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+<title>TP2 Ejercicio 3 </title>
+</head>
+<body>
+<script>
+var num = prompt("Ingrese un numero para mostrar su factorial");
+var resultado = 1
+
+for(var i=1; i<=num; i++) {
+    resultado *= i;
+
+}
+
+alert(resultado);
+
+</script>
+</body>
+</html>
