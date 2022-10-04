60d359a7791d3badd8c7670c15b981bc45b7763d
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:25:14 -0300

    Create ejercicio-10.js

---
+++ modulo01/tp02/ejercicio-10.js
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+<title>TP2 Ejercicio 10 </title>
+</head>
+<body>
+<script>
+
+var mes
+
+mes=parseFloat(prompt("Ingrese numero del mes"));
+
+
+if (mes == 1) {
+    document.write("El mes  "+mes+ "tiene  31 dias");
+
+}
+else if (mes==2){
+    document.write("El mes  "+mes+ "tiene  28 dias");
+
+}
+else if (mes==3){
+    document.write("El mes  "+mes+ "tiene  31 dias");
+
+}
+else if (mes==4){
+    document.write("El mes  "+mes+ "tiene  30 dias");
+
+}
+else if (mes==5){
+    document.write("El mes  "+mes+ "tiene  31 dias");
+
+}
+else if (mes==6){
+    document.write("El mes  "+mes+ "tiene  30 dias");
+
+}
+else if (mes==7){
+    document.write("El mes  "+mes+ "tiene  31 dias");
+
+}
+else if (mes==8){
+    document.write("El mes  "+mes+ "tiene  31 dias");
+
+}
+else if (mes==9){
+    document.write("El mes  "+mes+ "tiene  30 dias");
+
+}
+else if (mes==10){
+    document.write("El mes  "+mes+ "tiene  31 dias");
+
+}
+else if (mes==11){
+    document.write("El mes  "+mes+ "tiene  30 dias");
+
+}
+else if (mes==12){
+    document.write("El mes  "+mes+ "tiene  31 dias");
+
+}
+else {
+    document.write("Ingrese un numero de mes del 1 al 12");
+    
+}
+
+</script>
+</body>
+</html>
