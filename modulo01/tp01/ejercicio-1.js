c6b80ec49156b7140a98c9f5b4df5d55e6956548
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:13:19 -0300

    Create ejercicio-1.js

---
+++ modulo01/tp01/ejercicio-1.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <!-- indica en que formato se verá la pagina-->
+<head>
+   
+<title>Ejercicio 1 </title>
+<!-- se menciona el nombre que aparecerá en la pestaña del navegador-->
+</head>
+<body>
+    <!---- abre el cuerpo de la pagina-->
+<script>
+document.writeln("Hola Mundo!");
+<!-- solicita que muestre en pantalla el mensaje entre comillas -->
+document.write("<br>");
+<!--salta una linea-->
+document.writeln("Soy el primer script");
+
+</script>
+</body>
+<!--Cierra la pagina-->
+</html>
+<!--cierra la pagina-->
