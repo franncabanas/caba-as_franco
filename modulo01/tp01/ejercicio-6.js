ccd76dbabf5dbc4d434425259be343048c2175c0
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:16:00 -0300

    Create ejercicio-6.js

---
+++ modulo01/tp01/ejercicio-6.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> ejercicio 6
+        </title>
+    </head>
+    <body>
+        <script>
+            var dias=["Lunes","Martes", "Miercoles","Jueves", "Viernes", "Sabado", "Domingo"];
+            console.log(dias[0]);
+            console.log(dias[1]);
+            console.log(dias[2]);
+            console.log(dias[3]);
+            console.log(dias[4]);
+            console.log(dias[5]);
+            console.log(dias[6]);
+        </script>
+    </body>
+</html>
