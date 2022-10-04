4d6034c33291e4b53ebfaf036b8a6d2141656d34
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:24:45 -0300

    Update ejercicio-9.js

--- modulo01/tp02/ejercicio-9.js
+++ modulo01/tp02/ejercicio-9.js
@@ -2,27 +2,31 @@
 <html>
 <head>
    
-<title>TP2 Ejercicio 8 </title>
+<title>TP2 Ejercicio 9 </title>
 </head>
 <body>
 <script>
-var cantnum = 100
-var numero=[]
-while (numero.length<cantnum){
-    var numaleatorio = Math.ceil(Math.random()*cantnum);
-    var existe = false
-    for (var i=0; i<numero.length; i++){
-        if(numero[i]== numaleatorio){
-            existe= true;
-            break;
-        }
-    }
-    if (!existe){
-        numero[numero.length]=numaleatorio
 
-    }
+var nota1,nota2,nota3,promedio
+
+nota1=parseFloat(prompt("Ingrese primera calificacion"));
+nota2=parseFloat(prompt("Ingrese segunda calificacion"));
+nota3=parseFloat(prompt("Ingrese tercera calificacion"));
+promedio=(nota1+nota2+nota3)/3;
+
+if (promedio <= 5) {
+    document.write("El promedio es "+promedio+ "Reprobado");
+
 }
-document.write("numeros aleatorios : " +numero);
+else if (promedio<8){
+    document.write("El promedio es "+promedio+ "Aprobado");
+
+}
+else {
+    document.write("El promedio es "+promedio+ "Sobresaliente");
+    
+}
+
 </script>
 </body>
 </html>
