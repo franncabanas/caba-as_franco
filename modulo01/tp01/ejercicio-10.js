c7a6447a61f8f5729de053c345d561ce0858e319
Franco Cabañas <112988046+franncabanas@users.noreply.github.com>
2022-10-04 14:17:17 -0300

    Create ejercicio-10.js

---
+++ modulo01/tp01/ejercicio-10.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <head>
+        <title>Ejercicio 10 </title>
+        <script>
+
+            var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", " Octubre", "Noviembre", "Diciembre"];
+
+            var mes = window.prompt("Ingrese el número del Mes que desea conocer: ");
+
+            document.writeln(meses[(parseInt(mes) - 1)]);
+
+
+        </script>
+    </head>
+
+</body>
+
+</html>
